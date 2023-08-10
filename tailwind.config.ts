40b555575cd583a6dcb93a3346502010b14318e2
Isadora Pilau <isadorapda@gmail.com>
2023-08-10 14:32:47 +0100

    feat: 🎸 add global styles and theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,34 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkPrimaryColor: '#383838',
+        darkContrastPrimaryColor: '#FCFCFD',
+
+        primaryColor: '#FCFCFD',
+        contrastPrimaryColor: '#383838',
+
+        primaryBackground: '#EDFBED',
+        secondaryBackground: '#FCFCFD',
+        darkPrimaryBackground: '#383838',
+        darkSecondaryBackground: '#313030',
+        tertiaryColor: '#57A743',
+      },
+      fontFamily: {
+        roboto: ['var(--font-roboto)'],
+        exo: ['var(--font-exo)'],
+        michroma: ['var(--font-michroma)'],
+      },
+      fontSize: {
+        textBody: '16px',
+        textBodyMobile: '14px',
+        textHeaderSmall: '20px',
+        textHeadersLarge: '10vw',
+      },
+    },
   },
   plugins: [],
 }
